0a761094ec278e72f6089e6cd072b9b0056343cc
bthneee <107825365+bthneee@users.noreply.github.com>
2022-10-03 15:46:56 -0700

    Update 6 - factorCount.cpp

--- PE04/6 - factorCount.cpp
+++ PE04/6 - factorCount.cpp
@@ -2,13 +2,12 @@ int factorCount(int n,int& count)
 {
     count = 1; 
     int largest = 1; 
-    for (int i =1; i < n -1; ++i)
+    for (int i = 1; i < n - 1; ++i)
     {
-        if(n%i == 0)
+        if(n % i == 0)
         {
             largest = i;
             count++;
-            
         }
     }
     if(largest == 1) {return -1;}
