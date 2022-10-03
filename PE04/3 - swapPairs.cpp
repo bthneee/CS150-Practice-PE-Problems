6542e5264ac905b1b372479e575dd9d534b693c0
bthneee <107825365+bthneee@users.noreply.github.com>
2022-10-03 15:32:54 -0700

    Update 3 - swapPairs.cpp
    Added comments

--- PE04/3 - swapPairs.cpp
+++ PE04/3 - swapPairs.cpp
@@ -5,12 +5,25 @@ using namespace std;
 int swapPairs(string& s)
 {   int count = 0;
     int n, i;
-    if(s.size()%2 == 1) {n = s.size()-1;}
-    else {n = s.size();}
-    for(i=0; i<n; i += 2)
-    {   char temp = s[i];
-        s[i] = s[i+1];
-        s[i+1] = temp;
-        count++;        }
+ 
+    // if s.size() is odd
+    if(s.size() % 2 == 1) 
+    {
+        n = s.size() - 1;
+    }
+    // if s.size() is even
+    else 
+    {
+        n = s.size();
+    }
+    
+    // swap
+    for(i = 0; i < n; i += 2)
+    {   
+        char temp = s[i];
+        s[i] = s[i + 1];    // swaps places with next char in string
+        s[i + 1] = temp;
+        count++;            // counts # of swaps
+    }
     return count;
 }
