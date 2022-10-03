f858d68cdbf5bda1f3cfde6d81e3e2f95d5ff066
bthneee <107825365+bthneee@users.noreply.github.com>
2022-10-03 15:42:33 -0700

    Update 5 - nameDiamond.cpp

--- PE04/5 - nameDiamond.cpp
+++ PE04/5 - nameDiamond.cpp
@@ -1,19 +1,24 @@
 string nameDiamond(const string& s)
 {
    string name = "";
+   
+   // run through each char in string
    for(size_t i = 0; i < s.size();i++)
    {
-       for(size_t j=0;j <= i;j++)
+       for(size_t j = 0;j <= i; j++)
        {
+          //print letters in name one at a time
            name += s[j];
        }
+       // new line
        name += "\n";
    }
-   for(size_t i=1;i<s.size();i++)
+   
+   for(size_t i = 1;i < s.size(); i++)
    {
-       for(size_t j=0;j<s.size();j++)
+       for(size_t j = 0;j < s.size(); j++)
        {
-           if(j>=i)
+           if(j >= i)
            {
                name += s[j];
            }
