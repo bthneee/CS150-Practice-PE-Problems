a697eecacde9ba8f98bf659e57c6a969fadf36b8
bthneee <107825365+bthneee@users.noreply.github.com>
2022-10-03 15:57:00 -0700

    Update 9 - quadratic.cpp

--- PE04/9 - quadratic.cpp
+++ PE04/9 - quadratic.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 void quadratic(int a, int b, int c, double& root1, double& root2) 
 {
-    // formula under the square root
+    // formula under the square root: b^2 - 4 * a * c
     double d = b * b - 4 * a * c;
-    // square root
+    // square root 
     d = pow(d, 0.5);
     // add
     root1 = (-b + d) / (2 * a);
@@ -15,11 +15,11 @@ void quadratic(int a, int b, int c, double& root1, double& root2)
 }
 
 int main() {
-    int a, b, c;
+    // ax^2 + bx + c
+    int a = 1, b = -3, c = -4;
     double r1, r2;
-    cout << "Enter three coefficients: ";
-    cin >> a >> b >> c;
     quadratic(a, b, c, r1, r2);
     cout << "Roots are " << r1 << " and " << r2 << endl;
+        // r1 = 4, r2 = -1
     return 0;
 }
